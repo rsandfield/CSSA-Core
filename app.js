6c3a74ec43b6270660fb4f92ddf1a3cb83e0cf3f
Robert Sandfield <robert.f.sandfield@gmail.com>
2021-11-16 12:46:08 -0800

    Added common dummy test values

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 module.exports = {
     UrlCompleter: require('./models/urlCompleter'),
     errors: require('./models/errors'),
-    testHelper: require('./test/helper'),
     authClientFacing: require('./models/authClientFacing'),
     authServerFacing: require('./models/authServerFacing'),
+    testHelper: require('./test/helper'),
+    dummyValues: require('./models/dummyValues')
 }
